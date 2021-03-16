103d3abbab00df5fedf957254321dfe0f8d62cd1
cpach96 <cpach96@gmail.com>
2021-03-16 12:48:20 -0400

    commit tree 727b6e18026079ef6d01f4ea84c91ba0005d4153

--- src/index.js
+++ src/index.js
@@ -12,4 +12,31 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetch("http://localhost:3000/toys")
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(object){
+      let collection = document.getElementById("toy-collection")
+      object.forEach(toy => {
+      let cardClass = document.createElement("div")
+      let toyName = document.createElement("h2")
+      let img = document.createElement("img")
+      let likes = document.createElement("p")
+      let likeBtn = document.createElement("button")
+      img.src = toy.image
+      img.className = "toy-avatar"
+      cardClass.className = "card" 
+      likeBtn.className = "like-btn"
+      collection.appendChild(cardClass)  
+      toyName.innerText = toy.name
+      likes.innerText = toy.likes
+      cardClass.appendChild(toyName)
+      cardClass.appendChild(img)
+      cardClass.appendChild(likes)
+      cardClass.appendChild(likeBtn)
+      console.log(toy.image)
+      })
+    })
 });
+
